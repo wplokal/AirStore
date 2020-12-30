8a3a9ed71b52061f43bf1b7fafed29782c0bc2fb
wpexpert <sosmedlangit@gmail.com>
2020-12-30 22:26:05 +0700

    add ppdb sample

---
+++ wp-content/plugins/AirSchool-PPDB/ppdb.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Penerimaan Peserta Didik Baru ( PPDB ) -  Addons AirSchool
+Description: CIFOR Donor with custome table
+Version:     1.0
+Author:      Dodi hidayatullah
+Author URI:  https://github.com/wplokal
+*/
